aeb81a07294ea8589da445ea7fbcefbe83acc889
kaiserNikola <kaiserNikola@yandex.ru>
2016-03-06 23:35:33 +0300

    mysql

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Question(models.Model):
+	title = models.CharField(max_length=128, verbose_name='заголовок вопроса')
+	text  = models.TextField(verbose_name='полный текст вопроса')
+	added_at = models.DateTimeField(verbose_name='дата добавления вопроса', default=timezone.now)
+	rating   = models.IntegerField(verbose_name='рейтинг вопроса')
+	author   = models.ForeignKey(User, verbose_name='автор вопроса', related_name='questions')
+	likes    = models.ManyToManyField(User, verbose_name='список пользователей, поставивших "лайк"', related_name='liked_questions')
+
+
+class Answer(models.Model):
+	text     = models.TextField(verbose_name='текст ответа')
+	added_at = models.DateTimeField(verbose_name='дата добавления ответа', default=timezone.now)
+	question = models.ForeignKey(Question, verbose_name = 'вопрос, к которому относится ответ', related_name='answers')
+	author   = models.ForeignKey(User, verbose_name='автор ответа.')
+
--- init.sh
+++ init.sh
@@ -1,6 +1,16 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+
+sudo pip3 install gunicorn django mysqlclient
+
+sudo pip3 install django mysqlclient
+
+mysql -u root -e "create database stepic;"
+mysql -u root -e "CREATE USER 'box'@'localhost' IDENTIFIED BY '1234';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON box_django.* TO 'box'@'localhost';"
+
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 
