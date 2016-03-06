7a406d8f858f99da4fc8573248f80f1efbba6019
kaiserNikola <kaiserNikola@github.com>
2016-03-06 20:59:31 +0000

    fixes from server

--- init.sh
+++ init.sh
@@ -1,11 +1,16 @@
+git config --global user.email "kaiserNikola@github.com"                              
+git config --global user.name "kaiserNikola"             
+
+
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
+sudo apt-get update
+sudo apt-get install python3-dev libmysqlclient-dev
 
 sudo pip3 install gunicorn django mysqlclient
 
-sudo pip3 install django mysqlclient
 
 mysql -u root -e "create database stepic;"
 mysql -u root -e "CREATE USER 'box'@'localhost' IDENTIFIED BY '1234';"
